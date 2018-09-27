ac023001ee684ce3cba036fb2f3a0b676becead0
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2018-09-27 12:55:02 +0000

    Created a resizable button around orientation and size

--- boxbutton.py
+++ boxbutton.py
@@ -4,7 +4,10 @@ class BoxButton(urwid.WidgetWrap):
 	_border_char = u'-'
 	padding_sizes = { 'sm' : 2 , 'lg' : 6 }
 	btn_types = { 'menu': 'menu', 'action':'action' }
-	def __init__(self, label, on_press=None, user_data=None, size='sm', btn_type='action'):
+	btn_orientations = { 'square': 'square', 'vertical': 'vertical' }
+	def __init__(self, label, on_press=None,
+		user_data=None, size='sm', btn_type='action',
+		btn_orientation='square'):
 		border = self._border_char * (len(label) + self.padding_sizes[size] * 2)
 		cursor_position = len(border) + self.padding_sizes[size]
 		self.padding = self.padding_sizes[size] * ' '
@@ -14,12 +17,16 @@ class BoxButton(urwid.WidgetWrap):
 		_inner_widgets.append(urwid.Text(self.top[:-1]))
 		self.padding_line = u'|' + self.padding + len(label) * ' ' + self.padding + u'|'
 		self.middle = u'|' + self.padding + label + self.padding + u'|'
-		for  cnt in range((self.padding_sizes[size] // 2)):
+		no_of_paddings = self.padding_sizes[size] if btn_orientation == self.btn_orientations['vertical']\
+			else self.padding_sizes[size] // 2 if btn_orientation == self.btn_orientations['square']\
+			else 0
+		visible_cursor_position = self.padding_sizes[size] + 1
+		for  cnt in range(no_of_paddings):
 			_inner_widgets.append(urwid.Text(self.padding_line))
 		_inner_widgets.append(
-			urwid.AttrMap(urwid.SelectableIcon(self.middle, 3),
+			urwid.AttrMap(urwid.SelectableIcon(self.middle, visible_cursor_position),
 				'unselected', focus_map=self.btn_types[btn_type]))
-		for  cnt in range((self.padding_sizes[size] // 2)):
+		for  cnt in range(no_of_paddings):
 			_inner_widgets.append(urwid.Text(self.padding_line))
 		self.bottom = u'\u2514' + border + u'\u2518\n'
 		_inner_widgets.append(urwid.Text(self.bottom[:-1]))
@@ -48,7 +55,8 @@ if __name__ == '__main__':
 	# the only way to raise an Exception in lambda is through exec
 	onclick = lambda x: exec('raise urwid.ExitMainLoop()')
 	body = urwid.LineBox(urwid.Filler(
-		urwid.Columns([BoxButton('OK', on_press=onclick, size='lg', btn_type='menu'), 
+		urwid.Columns([BoxButton('OK', on_press=onclick, size='lg',
+			btn_type='menu', btn_orientation='horizontal'),
 		BoxButton('Exit', on_press=onclick)])
 	))
 	palette = [
